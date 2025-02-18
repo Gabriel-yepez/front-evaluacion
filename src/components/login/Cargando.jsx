0dd064914f4088fb344b297bdd90c67d952d7b07
Gabriel-yepez <125819572+Gabriel-yepez@users.noreply.github.com>
2025-02-17 20:26:59 -0400

    ajuste de carga

--- src/components/login/Cargando.jsx
+++ src/components/login/Cargando.jsx
@@ -3,7 +3,7 @@ export default function Cargando() {
   return (
     <div className="flex h-screen justify-center items-center bg-white">
       <div className="flex flex-col items-center">
-        <div className="loader-circle relative w-24 h-24 mb-4">
+        <div className="loader-circle relative w-24 h-24 mb-9">
           <div className="dot absolute bg-indigo-500 w-6 h-6 rounded-full"></div>
           <div className="dot absolute bg-indigo-500 w-6 h-6 rounded-full"></div>
           <div className="dot absolute bg-indigo-500 w-6 h-6 rounded-full"></div>
